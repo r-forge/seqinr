4467c5925569b667d0d12c4e5ca5cf108f3d18c8
lobry (Pr. ) <lobry@biomserv.univ-lyon1.fr>
2003-04-25 08:34:25 +0000

    Ooops ! A right parenthesis was missing
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqinr@20 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/seqinr/R/read.fasta.R
+++ trunk/seqinr/R/read.fasta.R
@@ -1,15 +1,15 @@
-read.fasta<-function(Fichier)
+read.fasta <- function(Fichier = system.file("sequences/ct.fasta", package = "package"))
 {
-seq<-scan(Fichier,what=character(),sep="\t") 
-ind<-c(grep(">",seq),length(seq)+1)
-nomseq<-as.list(c(rep(0,(length(ind)-1))))
-sequences<-as.list(rep(0,length(ind)-1))
-for(i in 1:(length(ind)-1))
-{
-nomseq[[i]]<-unlist(strsplit(seq[ind[i]]," "))[1]
-sequences[[i]]<-unlist(strsplit(seq[(ind[i]+1):(ind[i+1]-1)],split=NULL))
-}
-names(sequences)<-nomseq
-sequences
+  seq <- scan(Fichier,what=character(),sep="\t") 
+  ind <- c(grep(">",seq),length(seq)+1)
+  nomseq <- as.list(c(rep(0,(length(ind)-1))))
+  sequences <- as.list(rep(0,length(ind)-1))
+  for(i in 1:(length(ind)-1))
+  {
+    nomseq[[i]]<-unlist(strsplit(seq[ind[i]]," "))[1]
+    sequences[[i]]<-unlist(strsplit(seq[(ind[i]+1):(ind[i+1]-1)],split=NULL))
+  }
+  names(sequences)<-nomseq
+  sequences
 }
 
