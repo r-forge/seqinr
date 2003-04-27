4372e11d2888ebdcdd204ad579c742563a636469
lobry (Pr. ) <lobry@biomserv.univ-lyon1.fr>
2003-04-27 14:50:40 +0000

    db.growth.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqinr@39 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/seqinr/R/db.growth.R
+++ trunk/seqinr/R/db.growth.R
@@ -36,7 +36,8 @@ get.db.growth <- function()
   return(tmp)
 }
 
-dia.db.growth <- function( get.db.growth.out = get.db.growth() )
+dia.db.growth <- function( get.db.growth.out = get.db.growth(), 
+  Moore = TRUE, ... )
 {
   op <- par(no.readonly = TRUE)
   par( bg = "blue" )
@@ -49,12 +50,32 @@ dia.db.growth <- function( get.db.growth.out = get.db.growth() )
   attach( get.db.growth.out )
   plot( date, log10(Nucleotides) , pch = 20,
     main = "The exponential growth of the DDBJ/EMBL/Genbank content",
-    xlab = "Year", ylab = "Log10 number of nucleotides" )
+    xlab = "Year", ylab = "Log10 number of nucleotides", ... )
   abline(lm(log10(Nucleotides)~date),col="yellow")
   lm1 <- lm(log(Nucleotides)~date)
   mu <- lm1$coef[2] # slope
   dbt <- log(2)/mu # doubling time
   dbt <- 12*dbt # in months
-  text( x = 1995, y = 7, lab=paste("Doubling time:", round(dbt,1),"months"))
+
+  if( Moore )
+  {
+    x <- mean(date)
+    y <- mean(log10(Nucleotides))
+    a <- log10(2)/1.5
+    b <- y - a*x
+
+    lm10 <- lm(log10(Nucleotides)~date)
+    for( i in seq(-10,10,by=0.5) )
+      if( i != 0 )
+        abline( coef=c(b+i, a), col="black" )
+    legend( x = 1990, y = 7, leg= c(paste("Observed doubling time:", 
+      round(dbt,1),"months"),"Moore's doubling time : 18 months"), 
+      lty = c(1,1), col = c("yellow","black"))
+  }
+  else
+  {
+    legend( x = 1990, y = 7, leg=paste("Observed doubling time:", 
+      round(dbt,1),"months"), lty = 1, col = "yellow")
+  }
   par( op )
 } 
