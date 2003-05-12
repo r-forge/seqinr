72b080e44f7ae2d0ac48db613342c430954b6d6c
charif <unknown@noreply.com>
2003-05-12 15:36:29 +0000

    modification mineure
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqinr@60 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/seqinr/R/translate.R
+++ trunk/seqinr/R/translate.R
@@ -1,8 +1,7 @@
 translate<-function(sq,phase=0,sens="+",numcode=1)
 {
-	sq=toupper(unlist(strsplit(seq,"")))
-	if(sens=="-") sq<-comp(Inv.seq(sq))
-	else if(sens=="+") sq<-sq
+	if(sens=="-") sq<-comp(invers(sq))
+	else if(sens=="+") 
 	sq<-replace(sq,sq=="A",2)
 	sq<-replace(sq,sq=="C",1)
 	sq<-replace(sq,sq=="G",3)
@@ -13,7 +12,7 @@ translate<-function(sq,phase=0,sens="+",numcode=1)
 	b<-seq(phase+2,l+phase,3)
 	c<-seq(phase+3,l+phase,3)
 	tra<-16*sq[a]+4*sq[b]+sq[c]+1
-	code<-unlist(strsplit(as.vector(codes[numcode,]$V2),""))	
+	code<-unlist(strsplit(as.vector(CODES[numcode,]$V2),""))	
 	code[tra]
 }
 
