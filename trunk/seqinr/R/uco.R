e023ebe64d266d2011a73dcffa234e6a6b2e5cfc
charif <unknown@noreply.com>
2003-05-13 15:06:50 +0000

    I changed variable's name and i added names for the dataframe.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqinr@64 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/seqinr/R/uco.R
+++ trunk/seqinr/R/uco.R
@@ -1,8 +1,12 @@
-uco<-function(seq,phase=0,freq=FALSE,as.data.frame=FALSE){
-	sq<-splitseq(seq,phase,nombre=3)
+uco<-function( seq, frame = 0, freq = FALSE, as.data.frame = FALSE){
+	sq<-splitseq(seq,frame)
 	eff<-table(factor(sq,levels=CODON))
 	if(freq == TRUE) eff<-round(eff/(floor(length(seq)/3)),4)
-	if(as.data.frame == TRUE) eff=as.data.frame(cbind(as.character(AA),as.character(CODON),as.vector(eff)))
+	if(as.data.frame == TRUE){
+	eff=as.data.frame(cbind(as.character(AA),as.character(CODON),as.vector(eff)))
+	if(freq == TRUE) names(eff)=c("aa","codon","freq")
+	else names(eff)=c("aa","codon","eff")
+	}
 	eff
 } 
 
