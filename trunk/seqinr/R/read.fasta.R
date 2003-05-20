0255e8c3edcce7cfa2c88e90eb692f471895f74a
charif <unknown@noreply.com>
2003-05-20 14:12:42 +0000

    I added the class of the sequences
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqinr@92 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 342e9f221c78e424ca251889a050f6bbad68b45f

--- trunk/seqinr/R/read.fasta.R
+++ trunk/seqinr/R/read.fasta.R
@@ -1,16 +1,19 @@
-read.fasta <- function(Fichier = system.file("sequences/ct.fasta", package = "seqinr"))
+read.fasta <- function(File = system.file("sequences/ct.fasta", package = "seqinr"))
 {
-  seq <- scan(Fichier,what=character(),sep="\t") 
+  seq <- scan(File,what=character(),sep="\t") 
   ind <- c(grep(">",seq),length(seq)+1)
   nomseq <- as.list(c(rep(0,(length(ind)-1))))
   sequences <- as.list(rep(0,length(ind)-1))
+  lclass <- as.list(rep(0,length(ind)-1))
   for(i in 1:(length(ind)-1))
   {
     nomseq[[i]]<-unlist(strsplit(seq[ind[i]]," "))[1]
     sequences[[i]]<-unlist(strsplit(seq[(ind[i]+1):(ind[i+1]-1)],split=NULL))
   }
-  names(sequences)<-nomseq
-  sequences
+  nomseq = lapply(nomseq,substr,2,nchar(nomseq))
+  names(sequences)=nomseq
+  for(i in 1:length(sequences)) lclass[[i]]=sequences[i]
+  lapply(lclass,initSeqFasta)
 }
 
 
