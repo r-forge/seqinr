9e3c1cded7952c8598d25eaee956336115fc5fef
charif <unknown@noreply.com>
2003-05-22 12:57:21 +0000

    I repared error I had introduiced
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqinr@104 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: ddafd51382fd654f9f71fdbf1a46989466beccef

--- trunk/seqinr/R/lib.R
+++ trunk/seqinr/R/lib.R
@@ -1,7 +1,7 @@
 .First.lib <- function(lib, pkg)
 {
   library.dynam("seqinr", pkg, lib)
-   file1 <- file.path(lib,pkg,"data",".SEQINR.UTIL.RData")
+   file1 <- file.path(lib,pkg,"data","SEQINR.UTIL.RData")
    load (file1,.GlobalEnv)
    require("methods")		
 }
