c916e8a1ea9efa4480ff835a026f2432c5e3118f
lobry (Pr. ) <lobry@biomserv.univ-lyon1.fr>
2003-06-05 13:31:54 +0000

    I have added a new function, dotchart.uco() to display a Cleveland dotplot for codon usage.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqinr@130 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: b6ba99461ce70de028d505ccba2301c5be68840b

--- trunk/seqinr/R/uco.R
+++ trunk/seqinr/R/uco.R
@@ -10,4 +10,44 @@ uco<-function( seq, frame = 0, freq = FALSE, as.data.frame = FALSE){
 	eff
 } 
 
+dotchart.uco <- function(x, numcode = 1, aa3 = TRUE, cex = 0.7, 
+  alphabet = s2c("TCAG"), pch = 21, gpch = 20, bg = par("bg"), 
+  color = par("fg"), gcolor = par("fg"), lcolor = "gray", 
+  xlim = c( 0, max(x)), main = NULL, xlab = NULL, ylab = NULL, ...)
+{
+  if( is.null(names(x)) ) names(x) <- words( alphabet = alphabet )
+#
+# General sorting 
+#
+  x <- sort(x)
+  labels <- names(x)
+  groups <- as.factor(translate(s2c(labels), numcode =  numcode))
+  gdata <- sapply(split(x, groups), sum)
+#
+# Now, sorting by aa order
+#
+  gordered <- rank(gdata)
+  xidx <- numeric(64)
 
+  for( i in 1:64 )
+  {
+    xidx[i] <- -0.01*i + gordered[groups[i]]
+  }
+
+  x <- x[order(xidx)]
+  labels <- names(x)
+  aa <- translate(s2c(labels), numcode =  numcode)
+  groups <- factor(aa, levels = unique(aa))
+  gdata <- sapply(split(x, groups), sum)
+
+  xlim <- c(0, max(gdata))
+  if( aa3 )
+  {
+    levels(groups) <- aaa(levels(groups))
+  }
+
+  dotchart(x = x, labels = labels, groups = groups, gdata = gdata,
+   cex = cex, pch = pch, gpch = gpch, bg = bg, color = color,
+   gcolor = gcolor, lcolor = lcolor, xlim = xlim, main = main, 
+   xlab = xlab, ylab = ylab, ...) 
+}
