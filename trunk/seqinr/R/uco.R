b947ca61818ee365a9eaba722e8a29efdf67d1ed
charif <unknown@noreply.com>
2003-07-24 12:52:47 +0000

    J'ai ot�e . devant l'objet SEQINR.UTIL...
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqinr@191 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: d971c6a4e396a2d4e29841df8b05c7360cd0bed9

--- trunk/seqinr/R/uco.R
+++ trunk/seqinr/R/uco.R
@@ -1,9 +1,9 @@
 uco<-function( seq, frame = 0, freq = FALSE, as.data.frame = FALSE){
 	sq<-splitseq(seq,frame)
-	eff<-table(factor(sq,levels=.SEQINR.UTIL$CODON.AA$CODON))
+	eff<-table(factor(sq,levels=SEQINR.UTIL$CODON.AA$CODON))
 	if(freq == TRUE) eff<-round(eff/(floor(length(seq)/3)),4)
 	if(as.data.frame == TRUE){
-	eff=as.data.frame(cbind(as.character(.SEQINR.UTIL$CODON.AA$AA),as.character(.SEQINR.UTIL$CODON.AA$CODON),as.vector(eff)))
+	eff=as.data.frame(cbind(as.character(SEQINR.UTIL$CODON.AA$AA),as.character(SEQINR.UTIL$CODON.AA$CODON),as.vector(eff)))
 	if(freq == TRUE) names(eff)=c("aa","codon","freq")
 	else names(eff)=c("aa","codon","eff")
 	}
