4118232086fa0b212481fcc62b069c69ec4fe05b
charif <unknown@noreply.com>
2003-07-24 12:52:19 +0000

    J'ai rajout�'attribut nom pour les classes de sequences SeqFastaAA et SeqFastadna.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqinr@190 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: a6f194e0631215949509afda43793ee90a684596

--- trunk/seqinr/R/read.fasta.R
+++ trunk/seqinr/R/read.fasta.R
@@ -4,16 +4,16 @@ read.fasta <- function(File = system.file("sequences/ct.fasta", package = "seqin
   ind <- c(grep(">",seq),length(seq)+1)
   nomseq <- as.list(c(rep(0,(length(ind)-1))))
   sequences <- as.list(rep(0,length(ind)-1))
-  lclass <- as.list(rep(0,length(ind)-1))
   for(i in 1:(length(ind)-1))
   {
     nomseq[[i]]<-unlist(strsplit(seq[ind[i]]," "))[1]
+    nomseq[[i]]=substr(nomseq[[i]],2,nchar(nomseq))	
     sequences[[i]]<-unlist(strsplit(seq[(ind[i]+1):(ind[i+1]-1)],split=NULL))
+    attributes(sequences[[i]])=list(name=nomseq[[i]])
   }
-  nomseq = lapply(nomseq,substr,2,nchar(nomseq))
   names(sequences)=nomseq
-  for(i in 1:length(sequences)) lclass[[i]]=sequences[i]
-  lapply(lclass,initSeqFasta)
+  if(deftype(sequences[[1]])=="AA") lapply(sequences,initSeqFastaAA) 
+  else lapply(sequences,initSeqFastadna)
 }
 
 
