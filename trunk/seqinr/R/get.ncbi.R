a57c18d011769b3841820e1e8f2ebb50c31af648
lobry (Pr. ) <lobry@biomserv.univ-lyon1.fr>
2003-10-08 09:25:39 +0000

    bactgensize.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqinr@216 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 437f5307f0cac5836678a4688c6a7cde3699943e

--- trunk/seqinr/R/get.ncbi.R
+++ trunk/seqinr/R/get.ncbi.R
@@ -103,3 +103,66 @@ ncbi.fna.url <- function( get.ncbi.out = get.ncbi() )
   }
   apply( get.ncbi.out, 1, build.url )  
 }
+
+#
+#  Try to build urls to access complete genome sequences data
+#  in genbank format from get.ncbi() output
+#
+
+ncbi.gbk.url <- function( get.ncbi.out = get.ncbi() )
+{
+  build.url <- function( x )
+  {
+    urlname = paste("ftp://ftp.ncbi.nih.gov/genomes/Bacteria/", x[1],
+                 "/",x[2], collapse="", sep="")
+  }
+  apply( get.ncbi.out, 1, build.url )  
+}
+#
+#  Try to build urls to access complete genome sequences data
+#  file *.ptt from get.ncbi() output
+#
+
+ncbi.ptt.url <- function( get.ncbi.out = get.ncbi() )
+{
+  build.url <- function( x )
+  {
+    ficname <- unlist(strsplit(x[2],"\\.")) # split prefix and suffix
+    ficname <- ficname[1] # keep prefix
+    ficname <- paste( ficname, ".ptt", collapse="", sep="")
+    urlname = paste("ftp://ftp.ncbi.nih.gov/genomes/Bacteria/", x[1],
+                 "/",ficname, collapse="", sep="")
+  }
+  apply( get.ncbi.out, 1, build.url )  
+}
+
+#
+# Try to get the number of cds and genome size
+#
+
+ncbi.stats <- function( get.ncbi.out = get.ncbi() )
+{
+  gbkurls <- ncbi.gbk.url( get.ncbi.out )
+  ptturls <- ncbi.ptt.url( get.ncbi.out )
+  get.genome.size <- function( url )
+  {
+    header <- readLines( url, n = 1 )
+    tmp <- unlist(strsplit(header, split=" "))
+    tmp <- tmp[nchar(tmp)>0]
+    tmp <- tmp[3]
+    as.integer(tmp)
+  }
+  get.n.prot <- function( url )
+  {
+    lines <- readLines( url, n = 3 )
+    lines <- lines[3]
+    lines <- unlist(strsplit(lines, split=" "))
+    print(lines[1])
+    as.integer(lines[1])
+  }
+
+  gsize <- sapply( gbkurls, get.genome.size)
+  nprot <- sapply( ptturls, get.n.prot )
+  data <- data.frame( cbind(get.ncbi.out[,1], gsize, nprot) )
+  return( data )
+}
--- trunk/seqinr/R/lib.R
+++ trunk/seqinr/R/lib.R
@@ -3,6 +3,5 @@
   library.dynam("seqinr", pkg, lib)
    file1 <- file.path(lib,pkg,"data","SEQINR.UTIL.RData")
    load (file1,.GlobalEnv)
-   require("methods")		
 }
 
