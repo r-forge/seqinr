de38afab8f48f7e5250992b9d25067a72d2d1379
charif <unknown@noreply.com>
2003-10-24 12:15:15 +0000

    util.c
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqinr@233 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 39e8c6fdeba2fd9207e89347cb09c157160f33f2

---
+++ trunk/seqinr/src/util.c
@@ -0,0 +1,63 @@
+#include <Rinternals.h>
+#include <R.h>
+#include <Rdefines.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+
+
+/*##################################################*/
+/*# Convertir une string en vecteur de caractères  #*/
+/*##################################################*/
+
+
+SEXP s2c(SEXP seq){
+  char *string;
+  int lseq,i;
+  char mot[2];
+  
+  
+  SEXP chaine;
+
+  string = CHAR(STRING_ELT(seq,0));
+  
+  lseq = strlen(string);
+  
+  PROTECT(chaine=NEW_CHARACTER(lseq));
+
+
+  for(i=0;i<lseq;i++){  
+    mot[0]=toupper(string[i]);
+    mot[1]='\0';
+    SET_ELEMENT(chaine,i,mkChar(mot));
+    }
+  UNPROTECT(1);
+  return(chaine);
+}
+
+
+/*##################################################*/
+/*# Mettre de nouvelles variables d'environnement  #*/
+/*##################################################*/
+
+
+void Sysputenv(char **varname, char **valeur){
+  
+  int i,l;  
+  char *vartot;
+ 
+  l=strlen(*varname)+strlen(*valeur)+2;
+  vartot = malloc(l);
+  strcpy(vartot,*varname); 
+  strcat(vartot,"=");
+  strcat(vartot,*valeur);
+  printf("%s\n",vartot);
+  i=putenv(vartot);
+  printf("%d\n",i);
+}
+
+ 
+
+
