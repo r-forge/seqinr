b801e74e9406c19af3b1e477d571a9c58d254e19
charif <unknown@noreply.com>
2004-04-02 09:16:54 +0000

    *** empty log message ***
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqinr@360 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 3e5423ead304554faf538d96b084aa09424cba82

--- trunk/seqinr/R/lib.R
+++ trunk/seqinr/R/lib.R
@@ -4,11 +4,12 @@
   file1 <- file.path(lib,pkg,"data","SEQINR.UTIL.RData")
   load (file1,.GlobalEnv)
   load (file1,envir=as.environment(match("package:seqinr", search())))
+  if(exists("bankname",envir=globalenv())) rm(bankname,envir=globalenv())
   #if (!require(ade4))
      # stop("seqinr requires ade4, but ade4 couldn't be loaded")
 }
 
 .Last.lib <- function(libpath=.path.package(package = "seqinr"))
 {
-  if(exists(bankname)) rm(bankname)
+  if(exists("SEQINR.UTIL",envir=globalenv())) rm(SEQINR.UTIL,envir=globalenv())
 }
