14e70a835c1c56b8c64efaf6101d81128d9084d9
charif <unknown@noreply.com>
2004-05-18 13:29:19 +0000

    lib.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqinr@372 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 889059b33d26fcaf6c3f32498ce34d997ff9dc51

--- trunk/seqinr/R/lib.R
+++ trunk/seqinr/R/lib.R
@@ -2,7 +2,7 @@
 {
   library.dynam("seqinr", pkg, lib)
   file1 <- file.path(lib,pkg,"data","SEQINR.UTIL.RData")
-  file2 <- file.path(lib,pkg,"data","EXP.RData"
+  file2 <- file.path(lib,pkg,"data","EXP.RData")
  
   load (file1,envir=as.environment(match("package:seqinr", search())))
   load (file2,envir=as.environment(match("package:seqinr", search())))
