84fceb10a7d68d10c861a4cbe0eeb098ad0ebf15
charif <unknown@noreply.com>
2004-09-10 12:36:53 +0000

    kaks
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqinr@437 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: d793220623c4940f1cb57413a994d9582b9f9356

--- branches/seqinr_cran/seqinr/R/kaks.R
+++ branches/seqinr_cran/seqinr/R/kaks.R
@@ -1,8 +1,11 @@
 kaks = function(x){
 	if(attr(x,"class") != "alignment") error("object x must be of class alignment")
-	x$nb = as.numeric(x$nb)
+	if(x$nb <= 1) return(0)
+	tmp<-x
 	l = .Call("kaks",x$seq,x$nb,PACKAGE="seqinr")
-	m = lapply(l,function(k){
+	tmp$seq<-l[[5]]
+	assign(as.character(as.list(match.call())$x),tmp, envir = globalenv())
+	m = lapply(l[1:4],function(k){
 	if(! is.null(x$nam)) tmp = matrix( k, x$nb, x$nb, byrow = TRUE, dimnames=list(x$nam,x$nam))
 	else{
 	 n = paste("seq",c(1:x$nb),sep="")
@@ -10,7 +13,9 @@ kaks = function(x){
 	}
 	 as.dist(t(tmp))
 	})
-	m = lapply(m,round,digits=4)
-	names(m)=c("ks","ka","vks","vka")
+	m = lapply(m,round,digits=6)
+	names(m)=c("ka","ks","vka","vks")
 	return(m)
 }
+
+
