7f3538c4bfa887cea862f0c2859fc8a6e8682edf
lobry (Pr. ) <lobry@biomserv.univ-lyon1.fr>
2006-02-11 10:09:03 +0000

    mode debug
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqinr@619 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 7561639504d1c39d585e37bb0bd3987008d187d8

--- branches/seqinr_cran/seqinr/R/kaks.R
+++ branches/seqinr_cran/seqinr/R/kaks.R
@@ -1,21 +1,73 @@
-kaks = function(x){
-	if(attr(x,"class") != "alignment") error("object x must be of class alignment")
-	if(x$nb <= 1) return(0)
-	tmp<-x
-	l = .Call("kaks",x$seq,x$nb,PACKAGE="seqinr")
-	tmp$seq<-l[[5]]
-	assign(as.character(as.list(match.call())$x),tmp, envir = globalenv())
-	m = lapply(l[1:4],function(k){
-	if(! is.null(x$nam)) tmp = matrix( k, x$nb, x$nb, byrow = TRUE, dimnames=list(x$nam,x$nam))
-	else{
-	 n = paste("seq",c(1:x$nb),sep="")
-	 tmp = matrix( k, x$nb, x$nb, byrow = TRUE, dimnames=list(n,n))
-	}
-	 as.dist(t(tmp))
-	})
-	m = lapply(m,round,digits=6)
-	names(m)=c("ka","ks","vka","vks")
-	return(m)
+kaks <- function(x, debug = FALSE){
+    #
+    # Check argument class:
+    #
+    if(attr(x,"class") != "alignment") error("object x must be of class alignment")
+    if(debug){
+      cat("<--- Argument x storage is --->\n")
+      print(str(x))
+      cat("<--- Argument x storage is --->\n")
+    }
+    #
+    # Check that there are at least two sequences in the alignment:
+    #
+    if(x$nb <= 1) return(0)
+    #
+    # Make a local copy of the alignment (why? should be explained):
+    #
+    tmp <- x
+    
+    #
+    # Call internal C function:
+    #
+    l <- .Call("kaks", x$seq, x$nb, debug, PACKAGE = "seqinr")
+    if(debug){
+      cat("<--- Result l storage is --->\n")
+      print(str(l))
+      cat("<--- Result l storage is --->\n")
+    }
+    #
+    # l[[5]] contains the sequences, so this is to copy the sequences into
+    # the seq component of tmp. But the sequences were already there since
+    # tmp is a local copy of x. I don't understand this, the alignment is
+    # not supposed to be modified by kaks !!! This should be explained.
+    #
+    tmp$seq <- l[[5]]
+    #
+    #
+    #
+    if(debug) print(as.character(as.list(match.call())$x))
+    #
+    # Oh! oh! This is to modify the objet x in the user environment! Why
+    # why why ?? Extremly dangerous and undocumented. This may explain
+    # why two succesive calls to kaks do not yield the same result.
+    #
+    assign(as.character(as.list(match.call())$x), tmp, envir = globalenv())
+    if(debug){
+      cat("<--- tmp storage is --->\n")
+      print(str(tmp))
+      cat("<--- tmp storage is --->\n")
+    }
+    #
+    # This is to compute the list of results:
+    #
+    mkresult <- function(k){
+      if(! is.null(x$nam)){
+        tmp <- matrix( k, x$nb, x$nb, byrow = TRUE, dimnames = list(x$nam,x$nam))
+      } else {
+        n <- paste("seq", c(1:x$nb), sep = "")
+        tmp <- matrix( k, x$nb, x$nb, byrow = TRUE, dimnames = list(n,n))
+      }
+      as.dist(t(tmp))
+    }
+    m <- lapply(l[1:4], mkresult)
+    
+    #
+    # There is no valid reason to round the results, this should be deleted:
+    #
+    # m = lapply(m,round,digits=6)
+    #
+    names(m) <- c("ka", "ks", "vka", "vks")
+    return(m)
 }
-
 
