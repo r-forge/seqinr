f0348ae57ce60562b765897ab757e73f78f76973
lobry (Pr. ) <lobry@biomserv.univ-lyon1.fr>
2006-02-13 14:31:57 +0000

    tmp est vire
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqinr@621 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: bc8b9b7fd5babe80320bf7bfb11b987b02ea7b5f

--- branches/seqinr_cran/seqinr/R/kaks.R
+++ branches/seqinr_cran/seqinr/R/kaks.R
@@ -13,11 +13,6 @@ kaks <- function(x, debug = FALSE){
     #
     if(x$nb <= 1) return(0)
     #
-    # Make a local copy of the alignment (why? should be explained):
-    #
-    tmp <- x
-    
-    #
     # Call internal C function:
     #
     l <- .Call("kaks", x$seq, x$nb, debug, PACKAGE = "seqinr")
@@ -25,28 +20,6 @@ kaks <- function(x, debug = FALSE){
       cat("<--- Result l storage is --->\n")
       print(str(l))
       cat("<--- Result l storage is --->\n")
-    }
-    #
-    # l[[5]] contains the sequences, so this is to copy the sequences into
-    # the seq component of tmp. But the sequences were already there since
-    # tmp is a local copy of x. I don't understand this, the alignment is
-    # not supposed to be modified by kaks !!! This should be explained.
-    #
-    tmp$seq <- l[[5]]
-    #
-    #
-    #
-    if(debug) print(as.character(as.list(match.call())$x))
-    #
-    # Oh! oh! This is to modify the objet x in the user environment! Why
-    # why why ?? Extremly dangerous and undocumented. This may explain
-    # why two succesive calls to kaks do not yield the same result.
-    #
-    assign(as.character(as.list(match.call())$x), tmp, envir = globalenv())
-    if(debug){
-      cat("<--- tmp storage is --->\n")
-      print(str(tmp))
-      cat("<--- tmp storage is --->\n")
     }
     #
     # This is to compute the list of results:
@@ -61,12 +34,6 @@ kaks <- function(x, debug = FALSE){
       as.dist(t(tmp))
     }
     m <- lapply(l[1:4], mkresult)
-    
-    #
-    # There is no valid reason to round the results, this should be deleted:
-    #
-    # m = lapply(m,round,digits=6)
-    #
     names(m) <- c("ka", "ks", "vka", "vks")
     return(m)
 }
