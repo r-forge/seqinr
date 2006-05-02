9ebe9f8237383a674b821cbdd610a90124a86e76
necsulea <unknown@noreply.com>
2006-05-02 07:17:34 +0000

    nomseq[[i]] au lieu de nomseq
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqinr@692 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 9ae7e621171694f45cb79f56706dc0bc12df93f5

--- branches/seqinr_cran/seqinr/R/R_fasta.R
+++ branches/seqinr_cran/seqinr/R/R_fasta.R
@@ -7,7 +7,7 @@ read.fasta <- function(File = system.file("sequences/ct.fasta", package = "seqin
   for(i in 1:(length(ind)-1))
   {
     nomseq[[i]]<-unlist(strsplit(seq[ind[i]]," "))[1]
-    nomseq[[i]]=substr(nomseq[[i]],2,nchar(nomseq))	
+    nomseq[[i]]=substr(nomseq[[i]],2,nchar(nomseq[[i]]))	
     sequences[[i]]<-s2c(paste(seq[(ind[i]+1):(ind[i+1]-1)],collapse=""))
     if(seqtype == "DNA"){ sequences[[i]]=tolower( sequences[[i]])}
     attributes(sequences[[i]])=list(name=nomseq[[i]],Annot=(seq[ind[i]]),class = switch(seqtype,"AA"="SeqFastaAA", "DNA"= "SeqFastadna"))
