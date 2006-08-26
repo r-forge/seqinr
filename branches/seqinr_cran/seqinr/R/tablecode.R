c97d05f434b845013a462b0ac2de86dc1eed3bd9
lobry (Pr. ) <lobry@biomserv.univ-lyon1.fr>
2006-08-26 10:53:41 +0000

    more typo options in LaTeX tables
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqinr@764 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 31305f6ff5d84f033d3575328e6d6940cc643b54

--- branches/seqinr_cran/seqinr/R/tablecode.R
+++ branches/seqinr_cran/seqinr/R/tablecode.R
@@ -3,7 +3,9 @@
 #
 
 tablecode <- function(numcode = 1, urn.rna = s2c("TCAG"), dia = FALSE,
-latexfile = NULL, label = latexfile, size = "normalsize", caption = NULL)
+latexfile = NULL, label = latexfile, size = "normalsize", caption = NULL,
+preaa = rep("", 64), postaa = rep("", 64), 
+precodon = preaa, postcodon = postaa)
 {
   aa1 <- a()
   aa3 <- aaa()
@@ -21,18 +23,25 @@ latexfile = NULL, label = latexfile, size = "normalsize", caption = NULL)
 #  
   if( ! is.null(latexfile) ) {
     Tfile <- file(latexfile, open = "w")
-    writeLines("\\begin{table}", Tfile)
-    writeLines("\\begin{center}", Tfile)
+    cat("\\begin{table}", file = Tfile, sep = "
+")
+    cat("\\begin{center}", file = Tfile, sep = "
+")
 #
 # Character size:
 #
-    writeLines(paste("{\\", size, sep = ""), Tfile)
+    cat(paste("{\\", size, sep = ""), file = Tfile, sep = "
+")
     
-    writeLines("\\begin{tabular}{*{13}{l}}", Tfile)
-    writeLines("\\hline", Tfile)
-    writeLines("\\\\", Tfile)
+    cat("\\begin{tabular}{*{13}{l}}", file = Tfile, sep = "
+")
+    cat("\\hline", file = Tfile, sep = "
+")
+    cat("\\\\", file = Tfile, sep = "
+")
     
-    ncodon <- 1 # codon rank as in words(alphabet = s2c("tcag"))
+    ncodon <- 1 # codon rank as in
+    #paste(paste(rep(s2c("tcag"), each = 16), s2c("tcag"), sep = ""), rep(s2c("tcag"), each = 4), sep = "")
     for( i in 0:3 )
     {
       for( j in 0:3 )
@@ -41,32 +50,45 @@ latexfile = NULL, label = latexfile, size = "normalsize", caption = NULL)
         {
           codon <- c(urn[i+1], urn[k+1], urn[j+1])
           codon.urn <- paste(urn.rna[i+1], urn.rna[k+1], urn.rna[j+1], sep = "", collapse = "")
-          aminoacid <- aa3[which(aa1 == translate(codon, numcode = numcode))]
+          codon.urn <- paste(precodon[ncodon], codon.urn, postcodon[ncodon], sep = "")
 
-          writeLines(paste(codon.urn, aminoacid, " &", sep = " & "), Tfile)
+          aminoacid <- aa3[which(aa1 == translate(codon, numcode = numcode))]
+          aminoacid <- paste(preaa[ncodon], aminoacid, postaa[ncodon], sep = "")
+
+          cat(paste(codon.urn, aminoacid, " &", sep = " & "), file = Tfile, sep = "
+")
           ncodon <- ncodon + 1
         }
-        writeLines("\\\\", Tfile)
+        cat("\\\\", file = Tfile, sep = "
+")
       }
-      writeLines("\\\\", Tfile)
+      cat("\\\\", file = Tfile, sep = "
+")
     }
-    writeLines("\\hline", Tfile)
-    writeLines("\\end{tabular}", Tfile)
+    cat("\\hline", file = Tfile, sep = "
+")
+    cat("\\end{tabular}", file = Tfile, sep = "
+")
 #
 # Caption:
 #
-    writeLines(caption, Tfile)
+    cat(caption, file = Tfile, sep = "
+")
 #
 # LaTeX label:
 #
-    writeLines(paste("\\label{", label, "}", sep = ""), Tfile)
+    cat(paste("\\label{", label, "}", sep = ""), file = Tfile, sep = "
+")
 #
 # End character size:
 #
-    writeLines("}", Tfile)
+    cat("}", file = Tfile, sep = "
+")
 
-    writeLines("\\end{center}", Tfile)
-    writeLines("\\end{table}", Tfile)
+    cat("\\end{center}", file = Tfile, sep = "
+")
+    cat("\\end{table}", file = Tfile, sep = "
+")
     close(Tfile)
     return(invisible(NULL))
   }
