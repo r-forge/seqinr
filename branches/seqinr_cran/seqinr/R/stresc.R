b3771d65670e1286122d3d9b145716a3fb33b36d
lobry (Pr. ) <lobry@biomserv.univ-lyon1.fr>
2006-09-10 10:14:09 +0000

    stresc
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqinr@809 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: b9236379fe4e01bc5c1d2d30ace0d89c1010f400

---
+++ branches/seqinr_cran/seqinr/R/stresc.R
@@ -0,0 +1,7 @@
+stresc <- function(string = "->\\<->{<->}<->$<->^<->_<->%<->#<->&<->~<-",
+     fromchar = s2c("\\{}$^_%#&~"),
+     tochar = c("$\\backslash$", "\\{", "\\}", "\\$", "\\^{}",
+                                "\\_", "\\%", "\\#", "\\&", "\\~{}"))
+{
+  c2s( sapply(s2c(string), function(x) ifelse(x %in% fromchar, tochar[which(x == fromchar)], x)))
+}
