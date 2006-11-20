b2b267e5122210d6f03b4a6bf481f16577cb3d9c
penel <unknown@noreply.com>
2006-11-20 16:26:18 +0000

    Source en C pour recuperer les donnees compressees a tarvers les sockets
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqinr@845 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: c3124771540b5864996921971618549d9be9aa1a

--- branches/seqinr_cran/seqinr/src/getzlibsock.c
+++ branches/seqinr_cran/seqinr/src/getzlibsock.c
@@ -1,27 +1,121 @@
 #include <R.h>
 #include <Rdefines.h>
+#include <Rinternals.h>
+#include <zlib.h>		/* needs to be before Rconnections.h */
+#include <Rconnections.h>
+#include <stdlib.h>
+# include <stdio.h>
+# include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
 
+void *prepare_sock_gz_r(int ncon );
+char *z_read_sock(void *v);
+int close_sock_gz_r(void *v);
+/*static void *extract_opaque = NULL;*/
 
-SEXP getzlibsock(SEXP sock , SEXP debug)
+#define R_EOF	-1	
+
+SEXP getzlibsock(SEXP sock, SEXP nmax, SEXP debug)
 {
 
-
+  void *extract_opaque;
   int debugon;
-  FILE *fsocket;
-
-
-
-  SEXP rka;
-  SEXP rks;
-  SEXP rvka;
-  SEXP rvks;
-  SEXP res;
-/*  SEXP lsequtil; The effective number of sites used, to be implemented */
-
+  int testc;
+  SEXP essai,essai2; 
+  int numsoc;
+  SEXP numsocinR;
+  
+  /*------- */
+  SEXP ans = R_NilValue, ans2;
+  int i,j, n, nn, nnn, ok, warn, nread, c;
+  Rconnection con = NULL;
+  Rboolean wasopen;
+  
+  /*-------*/
+  char *test;
+  char *res,*megares;
+  char *mot[30];
+  
+  
+  /*a modifier?*/
   debugon = INTEGER_VALUE(debug);
- /* totseqs = INTEGER_VALUE(nbseq);*/
-  prepare_sock_gz_r(fsocket);
-   
-  if(debugon) Rprintf("C> mode degug is on at C level\n");
+  n=INTEGER_VALUE(nmax);
+		  
+  if(!inherits(sock, "connection")) {
+  	Rprintf("Error!\n\n'con' is not a connection");}
+  else{
+  	if (debugon)
+		Rprintf("'con' is a connection...\n");
+	con = getConnection(asInteger(sock));
+	wasopen = con->isopen;
+	if(!wasopen) {
+		if(!con->open(con)) 
+			Rprintf("Error!\n\ncannot open the connection");
+		}
+	else { /* for a non-blocking connection, more input may have become available, so re-position */
+		if (debugon)
+			Rprintf("I can open the connection...\n");
+		if(con->canseek && !con->blocking)
+	    		con->seek(con, con->seek(con, -1, 1, 1), 1, 1);
+    		}
+	numsoc = asInteger(sock)+1;
+	if (debugon)
+   		printf("Socket number is %d....\n",numsoc);
+   	extract_opaque=prepare_sock_gz_r(numsoc);
+   	res=(char *)malloc(500*sizeof(char)); 
+   	res=z_read_sock(extract_opaque);
+	if( res != NULL){ 
+		if (debugon)
+   			printf("-->%s\n",res);
+   		if (strcmp(res,"code=0") == 0) {
+			if (debugon)
+   			printf("Socket answer is ok %s(%d)\n",res, strlen(res));
+			} 
+    		nn = (n < 0) ? 1000 : n; /* initially allocate space for 1000 lines */
+    		nnn = (n < 0) ? INT_MAX : n;
+    		PROTECT(ans = allocVector(STRSXP, nn));
+		nread=0;
+		/*nnn=50;*/
+		if (debugon)
+    			printf("n=%d, nn=%d,nnn=%d\n",n,nn, nnn);
+		while (res != NULL)  {
+			if (nread >=nnn) {
+				printf("Increasing memory...\n");
+	    			ans2 = allocVector(STRSXP, 2*nn);
+	    			for(i = 0; i < nn; i++)
+					SET_STRING_ELT(ans2, i, STRING_ELT(ans, i));
+	    			nn *= 2;
+	    			nnn=nn;
+	    			UNPROTECT(1); /* old ans */
+	    			PROTECT(ans = ans2);
+				};
+			if  (strcmp(res,"\033count=1") != 0){
+				SET_STRING_ELT(ans, nread, mkChar(res));
+				nread++;
+				}
+			res=z_read_sock(extract_opaque);
+    			}
+		if (debugon)
+			printf("Number of lines of data : %d\n",nread-2);
+    		ans2 = allocVector(STRSXP, nread-2);
+		j=0; /* Je remplis en evitanat la premiere et la derniere ligne*/
+    		for(i = 1; i < nread-1; i++){
+			SET_STRING_ELT(ans2, j, STRING_ELT(ans, i));
+			j++;
+			}
+	    	UNPROTECT(1); /* old ans */
+	    	PROTECT(ans = ans2);
+    		UNPROTECT(1);
+		if (debugon)
+     			printf("Ok, everything is fine!\n");
+		} 
+	else {
+		printf("Socket error!\n");
+		}
+    	testc=close_sock_gz_r(extract_opaque);
+	}
 
+  if(!wasopen) con->close(con);
+  return ans ;
 }
