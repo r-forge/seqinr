421ff0e5dd90e7dfbfdce31f499407dae07ba062
lobry (Pr. ) <lobry@biomserv.univ-lyon1.fr>
2007-04-12 21:06:58 +0000

    code polishing
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqinr@950 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 7c1a7c3c576ba8119e994f9e87c1a832bd079637

--- branches/seqinr_cran/seqinr/R/translate.R
+++ branches/seqinr_cran/seqinr/R/translate.R
@@ -1,29 +1,55 @@
 translate <- function(seq, frame = 0, sens = "F", numcode = 1, NAstring = "X")
 {
-	if(sens == "R") seq <- comp(invers(seq))
+  #
+  # Take the reverse complementary strand when required:
+  #
 
-	n <- c(a = 2, c = 1, g = 3, t = 0)
-    	seq <- as.vector(n[seq])
-    	
-	seq <- as.numeric(seq) 
-	
-	l <- floor((length(seq)-frame)/3)*3
-	
-	a <- seq(frame + 1, l + frame, 3)
-	b <- seq(frame + 2, l + frame, 3)
-	c <- seq(frame + 3, l + frame, 3)
-	
-	tra <- 16*seq[a] + 4*seq[b] + seq[c] + 1
-	
-	code <- unlist(strsplit(as.vector(SEQINR.UTIL$CODES.NCBI$CODES[numcode]),""))	
-	
-	result <- code[tra]
-	
-	result[is.na(result)] <- NAstring
-	
-	return( result )
-	
-	
+  if(sens == "R") seq <- comp(rev(seq))
+
+  #
+  # Transform the sequence in its numerical encoding equivalent
+  # with textbook order, that is t = 0, c = 1, a = 2, g = 3
+  #
+
+  seq <- s2n(seq, levels = s2c("tcag"))
+
+  #
+  # Compute the length of the sequence when its length in codons
+  # is an integer:
+  #
+  
+  l <- 3*((length(seq) - frame) %/% 3)
+
+  #
+  # Compute the indices for the first codon positions:
+  #
+  
+  c1 <- seq(from = frame + 1, to = frame + l, by = 3)
+  
+  #
+  # Compute the indices of codons in the translation table:
+  #
+
+  tra <-  16*seq[c1] + 4*seq[c1 + 1] + seq[c1 + 2] + 1
+
+  #
+  # Get the translation table:
+  #
+
+  code <- s2c(SEQINR.UTIL$CODES.NCBI$CODES[numcode])
+
+  #
+  # Translate the sequence:
+  #
+
+  result <- code[tra]
+
+  #
+  # Replace missing values by the string for missing amino-acids:
+  #
+
+  result[is.na(result)] <- NAstring
+
+  return( result )
 }
-
 
