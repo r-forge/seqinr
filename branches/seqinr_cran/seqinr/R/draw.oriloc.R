9c37f3543c35b90c97a33f5e950d3c919e31edaf
lobry (Pr. ) <lobry@biomserv.univ-lyon1.fr>
2007-04-13 16:57:57 +0000

    more user options
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqinr@958 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: f77204396618793d712686f4e4190eceba50ba30

--- branches/seqinr_cran/seqinr/R/draw.oriloc.R
+++ branches/seqinr_cran/seqinr/R/draw.oriloc.R
@@ -1,58 +1,70 @@
-draw.oriloc <- function(ori){
+draw.oriloc <- function(ori, main = "Title",
+  xlab = "Map position in Kb",
+  ylab = "Cumulated combined skew in Kb", las = 1, las.right = 3,
+  ta.mtext = "Cumul. T-A skew", ta.col = "pink", ta.lwd = 1,
+  cg.mtext = "Cumul. C-G skew", cg.col = "lightblue", cg.lwd = 1,
+  cds.mtext = "Cumul. CDS skew", cds.col = "lightgreen", cds.lwd = 1,
+  sk.col = "black", sk.lwd = 2,
+  add.grid = TRUE, ...){
 
-  ta=ori$x
-  cg=ori$y
-  skew=ori$skew
-  cdsskew=ori$CDS.excess
-
-  ## wrapped cds
-
-  start.kb=ori$start.kb
-  end.kb=ori$end.kb
-  
-
-  wrapped=which(abs(ori$start.kb-ori$end.kb)>=50) 
-  if(wrapped==length(cdsskew)){
-    start.kb[wrapped]=max(start.kb[wrapped],end.kb[wrapped])
-    end.kb[wrapped]=max(start.kb[wrapped],end.kb[wrapped])
+#
+# Get data and use Kb units for skews and map positions:
+#
+  ta <- ori$x/1000          # Cumulated T-A skew in Kb
+  cg <- ori$y/1000          # Cumulated C-G skew in Kb
+  skew <- ori$skew/1000     # Cumulated combined skew in Kb
+  cdsskew <- ori$CDS.excess # Cumulated CDS orientation bias
+  start.kb <- ori$start.kb
+  end.kb <- ori$end.kb
+#
+# This is to deal with CDS that wrapp around the genome:
+#
+  wrapped <- which(abs(ori$start.kb - ori$end.kb) >= 50) 
+  if(wrapped == length(cdsskew)){
+    start.kb[wrapped] <- max(start.kb[wrapped], end.kb[wrapped])
+    end.kb[wrapped] <- max(start.kb[wrapped], end.kb[wrapped])
   }
-  if(wrapped==1){
-    end.kb[wrapped]=min(start.kb[wrapped],end.kb[wrapped])
-    start.kb[wrapped]=min(start.kb[wrapped],end.kb[wrapped])
+  if(wrapped == 1){
+    end.kb[wrapped] <- min(start.kb[wrapped], end.kb[wrapped])
+    start.kb[wrapped] <- min(start.kb[wrapped], end.kb[wrapped])
   }
-     
-  meancoord=(start.kb+end.kb)/2
+#
+# Use CDS midpoints as x-coordinates:
+#
+  meancoord <- (start.kb + end.kb)/2
 
-  ymin <- min(ta,cg,skew)
-  ymax <- max(ta,cg,skew)
+  ymin <- min(ta, cg, skew)
+  ymax <- max(ta, cg, skew)
   xmin <- min(meancoord)
   xmax <- max(meancoord)
   
   ticks <- pretty(cdsskew)
   
-  ticks.y <- (ymax-ymin)/(max(cdsskew)-min(cdsskew))*(ticks - min(cdsskew)) + ymin
-  cds.y   <- (ymax-ymin)/(max(cdsskew)-min(cdsskew))*(cdsskew - min(cdsskew)) + ymin
+  ticks.y <- (ymax-ymin)/(max(cdsskew) - min(cdsskew))*(ticks - min(cdsskew)) + ymin
+  cds.y   <- (ymax-ymin)/(max(cdsskew) - min(cdsskew))*(cdsskew - min(cdsskew)) + ymin
 
- plot(meancoord, cg, type="l", xlab="Map position (gene index)",
-      ylab = "Cumulated normalized skew",xlim=c(xmin,xmax),ylim=c(ymin,ymax),cex.lab=1.35,col="lightblue",main="Rearranged nucleotide skews",lwd=1.5)
+  plot(meancoord, cg, type="l", xlab = xlab,
+      ylab = ylab, xlim = c(xmin, xmax), ylim = c(ymin, ymax),
+      cex.lab = 1.35, col = cg.col, main = main, lwd = cg.lwd, las = las, ...)
 
-  axis(side = 4, at = ticks.y, labels = ticks, col = "lightgreen", col.axis ="black")
+  axis(side = 4, at = ticks.y, labels = ticks, col = cds.col, las = las.right)
 
-  tmp <- pretty(meancoord)
-  abline(v=tmp, col="grey", lty=3)
-  tmp <- tmp[-length(tmp)] + diff(tmp)/2
-  abline(v=tmp, col="grey", lty=3)
+#
+# Add vertical grid when required:
+#
+  if(add.grid){
+    tmp <- pretty(meancoord)
+    abline(v = tmp, col = "grey", lty=3)
+    tmp <- tmp[-length(tmp)] + diff(tmp)/2
+    abline(v = tmp, col = "grey", lty=3)
+  }
   
-  lines(meancoord,ta, col="pink",lwd=1.5)
-
-  lines(meancoord,skew, col="black",lwd=1.5)
+  lines(meancoord, ta, col = ta.col, lwd = ta.lwd)
+  lines(meancoord, skew, col = sk.col, lwd = sk.lwd)
+  lines(meancoord, cds.y, col = cds.col, lwd = cds.lwd)
   
-  lines(meancoord,cds.y, col="lightgreen",lwd=1.5)
-  
-  mtext("Cumul. T-A skew", col="pink", adj=0)
-  mtext("Cumul. C-G skew", col="lightblue")
-  mtext("Cumul. CDS skew", col="lightgreen", adj=1)
-
- 
+  mtext(ta.mtext, col = ta.col, adj = 0)
+  mtext(cg.mtext, col = cg.col)
+  mtext(cds.mtext, col = cds.col, adj=1)
 
 }
