fc97b352ec4b41cb9b07fec6304c7d64c00ba1a6
lobry (Pr. ) <lobry@biomserv.univ-lyon1.fr>
2007-07-14 11:41:01 +0000

    partial matching and toupper
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqinr@1027 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 5ab59da2581911515aa373812d971aab02412376

--- branches/seqinr_cran/seqinr/R/dist.alignment.R
+++ branches/seqinr_cran/seqinr/R/dist.alignment.R
@@ -2,17 +2,35 @@
 # Pairwise Distances from Aligned Protein or DNA/RNA Sequences
 #
 
-dist.alignment <- function(x, matrix = "similarity" )
+dist.alignment <- function(x, matrix = c("similarity", "identity") )
 {
-  if (!inherits(x, "alignment")) 
-        stop("Object of class 'alignment' expected")
-  t1 <- c("similarity", "identity")
-  m1 <- grep(matrix, t1)
-  m2 <- as.numeric(.Call("is_a_protein_seq", x$seq[1], PACKAGE = "seqinr") >= 0.8)
-  l <- x$nb
-  maxseq <- length(x$seq)
-  dist <- .Call("distance", x$seq, l, m1, m2, PACKAGE = "seqinr")
-  mat <- matrix(dist, l, l, byrow = TRUE)
+  #
+  # Check arguments:
+  #
+  if (!inherits(x, "alignment")) stop("Object of class 'alignment' expected")
+  #
+  # Match arguments:
+  #
+  matrix <- match.arg(matrix)
+  #
+  # Compute arguments for the C distance function:
+  #
+  sequences <- toupper(x$seq)
+  nbseq <- x$nb
+  matNumber <-ifelse(matrix == "similarity", 1, 2)
+    #
+    # The following shouldn't be hard encoded, an argument for full
+    # user control should be added.
+    #
+  seqtype <- as.numeric(.Call("is_a_protein_seq", sequences[1], PACKAGE = "seqinr") >= 0.8)
+  #
+  # Call the C distance function:
+  #
+  dist <- .Call("distance", sequences, nbseq, matNumber, seqtype, PACKAGE = "seqinr")
+  #
+  # Convert the result in a object of class dist:
+  #
+  mat <- matrix(dist, nbseq, nbseq, byrow = TRUE)
   dimnames(mat) <- list(x$nam, x$nam)
   return( as.dist(mat) )
 }
