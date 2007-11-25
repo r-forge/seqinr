7e7943735f585372e830e3983a4a848b1975c3fd
lobry (Pr. ) <lobry@biomserv.univ-lyon1.fr>
2007-11-25 18:45:20 +0000

    perf optimization
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqinr@1298 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 797d90fa9479aaed3eb711c6c40af366542aed2c

--- branches/seqinr_cran/seqinr/R/read.fasta.R
+++ branches/seqinr_cran/seqinr/R/read.fasta.R
@@ -1,6 +1,6 @@
 read.fasta <- function(file = system.file("sequences/ct.fasta", package = "seqinr"), 
   seqtype = "DNA", File = NULL, as.string = FALSE, forceDNAtolower = TRUE,
-  set.attributes = TRUE, legacy.mode = TRUE)
+  set.attributes = TRUE, legacy.mode = TRUE, seqonly = FALSE)
 {
   #
   # Check arguments:
@@ -32,49 +32,43 @@ read.fasta <- function(file = system.file("sequences/ct.fasta", package = "seqin
     stop("no line starting with a > character found")
   }
   #
-  # Add an extra line so that the last sequence is not lost:
+  # Localize sequence data:
   #
-  ind <- c(ind, length(lines) + 1)
+  start <- ind + 1
+  end <- ind - 1
+  end <- c(end[-1], length(lines))
   #
-  # Make empty lists to store results:
+  # Read sequences:
   #
-  nomseq <- vector(mode = "list", length = nseq)
-  sequences <- vector(mode = "list", length = nseq)
+  sequences <- lapply(seq_len(nseq), function(i) paste(lines[start[i]:end[i]], collapse = ""))
+  if(seqonly) return(sequences)
   #
-  # Main loop to read sequences:
-  #  
-  for(i in seq_len(nseq))
-  {
-    #
-    # Sequence name ends with the first space character:
-    #
-    nomseq[[i]] <- unlist(strsplit(lines[ind[i]], " "))[1]
-    #
-    # Remove the first '>' character:
-    #
-    nomseq[[i]] <- substr(nomseq[[i]], 2, nchar(nomseq[[i]]))
-    #
-    # Read sequence:
-    #
-    sequences[[i]] <- paste(lines[(ind[i]+1):(ind[i+1]-1)], collapse = "")
-    #
-    # Turn it into a vector of single chars if required:
-    #
-    if( as.string == FALSE) sequences[[i]] <- s2c(sequences[[i]])
-    #
-    # Turn DNA sequences in lower case letters if required:
-    #
-    if(seqtype == "DNA"){
-      if(forceDNAtolower){
-        sequences[[i]] <- tolower( sequences[[i]])
-      }
+  # Read sequence names (unoptimized):
+  #
+  nomseq <- lapply(seq_len(nseq), function(i){
+    firstword <- strsplit(lines[ind[i]], " ")[[1]][1]
+    substr(firstword, 2, nchar(firstword))
+  })
+  #
+  # Turn DNA sequences in lower case letters if required:
+  #
+  if(seqtype == "DNA"){
+    if(forceDNAtolower){
+      sequences <- tolower(sequences)
     }
-    #
-    # Set sequence attributes when required:
-    #
-    if(set.attributes){
+  }
+  #
+  # Turn it into a vector of single chars if required:
+  #
+  if(as.string == FALSE) sequences <- lapply(sequences, s2c)
+  #
+  # Set sequence attributes when required:
+  #
+  if(set.attributes){
+    for(i in seq_len(nseq)){
       attributes(sequences[[i]]) <- list(name = nomseq[[i]], 
-        Annot = (lines[ind[i]]), class = switch(seqtype, "AA" = "SeqFastaAA", "DNA" = "SeqFastadna"))
+        Annot = (lines[ind[i]]),
+        class = switch(seqtype, "AA" = "SeqFastaAA", "DNA" = "SeqFastadna"))
     }
   }
   #
