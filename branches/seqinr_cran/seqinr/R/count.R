9d9e9a174bddfdbb1b73ee38ddc25218af9bc88f
lobry (Pr. ) <lobry@biomserv.univ-lyon1.fr>
2008-01-15 17:28:34 +0000

    more code documentation
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqinr@1358 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 83078b7a35a1814b5071c9e9ce8670e5a0720cbf

--- branches/seqinr_cran/seqinr/R/count.R
+++ branches/seqinr_cran/seqinr/R/count.R
@@ -1,14 +1,29 @@
 count <- function(seq, word, frame = 0, freq = FALSE, alphabet = s2c("acgt")){
-  l <- (length(seq) - (word + frame)) + 1
-  s <- seq[(frame+1):(frame+l)]
-  seq <- seq[(frame+1):length(seq)]
+#
+# l is the number of elements in the sequence when starting at "frame" position
+# and ending so that the last word is documented:
+#
+  l <- length(seq) - word - frame + 1
+#
+# s contains all words starting characters:
+#
+  s <- seq[(frame + 1):(frame + l)]
+#
+# After this statment seq contains all characters useful to build words:
+#
+  seq <- seq[(frame + 1):length(seq)]
   if (word == 1){
     counts <- table(factor(seq, levels = levels(as.factor(words(1, alphabet = alphabet)))))
-  }
-  else{
-    for(i in 2:word) s <- paste(s, seq[i:(i + l - 1)], sep = "")
+  } else {
+    for(i in 2:word){
+      #
+      # Here we build all words by pasting the following characters:
+      #
+      s <- paste(s, seq[i:(i + l - 1)], sep = "")
+    }
     counts <- table(factor(s, levels = levels(as.factor(words(word, alphabet = alphabet)))))
   }
+  
   if (freq == FALSE){
     return(counts)
   }
