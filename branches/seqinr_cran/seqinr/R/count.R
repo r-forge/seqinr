4a247819edb10cd51b6c67cb60b107e4b572acc5
lobry (Pr. ) <lobry@biomserv.univ-lyon1.fr>
2008-01-17 10:49:31 +0000

    phase -> start
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/seqinr@1361 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 146f7a0ba39eb97854f9c7c0d73b2d8317c320a6

--- branches/seqinr_cran/seqinr/R/count.R
+++ branches/seqinr_cran/seqinr/R/count.R
@@ -1,18 +1,18 @@
-count <- function(seq, word, phase = 0, freq = FALSE, alphabet = s2c("acgt"), frame = phase){
-  if(!missing(frame)) warning("argument frame is deprecated, use phase instead")
+count <- function(seq, word, start = 0, freq = FALSE, alphabet = s2c("acgt"), frame = start){
+  if(!missing(frame)) warning("argument frame is deprecated, use start instead")
 #
-# l is the number of elements in the sequence when starting at "phase" position
+# l is the number of elements in the sequence when starting at "start" position
 # and ending so that the last word is documented:
 #
-  l <- length(seq) - word - phase + 1
+  l <- length(seq) - word - start + 1
 #
 # s contains all words starting characters:
 #
-  s <- seq[(phase + 1):(phase + l)]
+  s <- seq[(start + 1):(start + l)]
 #
 # After this statment seq contains all characters useful to build words:
 #
-  seq <- seq[(phase + 1):length(seq)]
+  seq <- seq[(start + 1):length(seq)]
   if (word == 1){
     counts <- table(factor(seq, levels = levels(as.factor(words(1, alphabet = alphabet)))))
   } else {
